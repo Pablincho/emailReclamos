8db7ff57c08410b6b2d3ac8d311ad9b8b61f68bf
Pablo <pablodo@gmail.com>
2018-10-16 18:40:06 -0300

    Primer parseo codigo y mensaje

--- emailReclamos.py
+++ emailReclamos.py
@@ -10,6 +10,8 @@ USERNAME = "senseitarg@gmail.com"
 PASSWORD = "iotplatform"
 SERVER_NAME = "pop.gmail.com"
 PORT = 995
+ERR = -1
+INVALID_FORMAT = 'Error, formato no valido'
 
 class ReadMail(object):
 	def __init__(self, user, passw, server, port):
@@ -44,14 +46,23 @@ class ReadMail(object):
 		return raw_email
 
 class ParseEmail(object):
+	def parseBody(self, body):
+		#body = '3407 Dirigirse a Rivadeo 1486 por ascensor fuera de servicio'
+		firstBody = body.split('\n')[0]
+		#print(firstBody)
+		match = re.match(r"\d{4}",firstBody.strip())				# Busco si el mensaje empieza con 4 digitos numericos
+	
+		if(match):
+			code = match.group()							# Si es asi guardo la coincidencia encontrada
+			message = (re.sub(code,'',firstBody)).strip()
+			return code, message
+		else:	
+			logging.warning("Formato invalido")		
+			return ERR, INVALID_FORMAT
 
 	def parseEmail(self, raw_email):
 		for pos, value in enumerate(raw_email):
-			#print(value)
-			#formatedValue = b"\n".join(str(raw_email))
-			#print (formatedValue)
 			parsed_email = parser.Parser().parsestr(str(value))
-			#print(parsed_email)
 			Body = []
 			sUB = parsed_email['Subject']
 			print ("Info:Subject:", sUB)
@@ -59,23 +70,25 @@ class ParseEmail(object):
 			f = re.findall(r'[\w\.-]+@[\w\.-]+', (str(parsed_email['From'])))
 			#You can print other parameters as weel. Such as attachement, if yes, then you can download the attachment. 
 			print ("Mail From:", f[0])
-			print ("Thread-Index", parsed_email['Thread-Index'])
-			print ("In-Reply-To", parsed_email['In-Reply-To'])
-			print ("Message-ID", parsed_email['Message-ID'])
-			print ("References", parsed_email['References'])
+			#print ("Thread-Index", parsed_email['Thread-Index'])
+			#print ("In-Reply-To", parsed_email['In-Reply-To'])
+			#print ("Message-ID", parsed_email['Message-ID'])
+			#print ("References", parsed_email['References'])
 			if parsed_email.is_multipart() == False:
 				print("Info: This mail is not multilple lines.")
 				Final_body = parsed_email.get_payload()
 			else:
 				print("Info: Multiline in body")
 				for payload in parsed_email.get_payload():
-					print(payload.get_payload)
+					#print(payload.get_payload)
 					Body.append(payload.get_payload())
 				Final_body='\n'.join(Body)
-				#Final_body = '\n'.join(str(v) for v in Body)
-				#Body = parsed_email.get_payload()
-			print("Here is the Body contents : ",Final_body)
+			#print("Here is the Body contents : ",Final_body)
+			code, message = self.parseBody(Final_body)
+			print(code, message)
         	#pop_conn.quit()
+
+
 
 if __name__ == '__main__':
 	mail = ReadMail(USERNAME, PASSWORD, SERVER_NAME, PORT)
